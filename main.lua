475fb4fe62f4b14d907e0c6e25adf707672fe708
Austin Beeler <abeeler1@uncc.edu>
2014-11-12 09:53:36 -0500

    Added basic LOVE methods
    Comments describing functionality included

---
+++ main.lua
@@ -0,0 +1,15 @@
+-- Run once on startup, loads necessary files and data
+function love.load()
+
+end
+
+-- Update call for each frame
+-- dt = Time since last update (in seconds, will be a fraction)
+function love.update( dt )
+
+end
+
+-- Draw call for each frame
+function love.draw()
+
+end
